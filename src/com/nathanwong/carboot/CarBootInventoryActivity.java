e7c6878c127710d701a0b9937814ce1cb3bea74f
Nathan Wong <nathan@nathanwong.co.uk>
2012-05-12 23:29:20 +0100

    Add parse library
    Adds parse library and keeps it's secret keys away from git's prying
    eyes.

--- src/com/nathanwong/carboot/CarBootInventoryActivity.java
+++ src/com/nathanwong/carboot/CarBootInventoryActivity.java
@@ -3,11 +3,16 @@ package com.nathanwong.carboot;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.parse.Parse;
+
 public class CarBootInventoryActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Parse.initialize(this, getString(R.string.applicationId), getString(R.string.clientKey));
+
     }
 }
